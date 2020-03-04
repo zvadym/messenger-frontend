ea65b5e27b148153de7e48fe750bf10f950e0015
zvadym <v.zabiiaka@gmail.com>
2020-03-04 13:58:43 +0100

    Delete firebase (part 1)

--- src/services/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDoAqHbn8HZZRJA3YcdHhpsWc9IK_cc7yc',
-  authDomain: 'zvadym-vue-chat.firebaseapp.com',
-  databaseURL: 'https://zvadym-vue-chat.firebaseio.com',
-  projectId: 'zvadym-vue-chat',
-  storageBucket: 'zvadym-vue-chat.appspot.com',
-  messagingSenderId: '91797117240',
-  appId: '1:91797117240:web:07f7747c549d975965c9cd'
-}
