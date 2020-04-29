c6b854efc34590a23280c2a5d3b6c64f4dae8df7
zvadym <v.zabiiaka@gmail.com>
2020-04-29 18:00:58 +0300

    UserAvatar layout changes

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="avatar">
-    <v-avatar size="30" color="red" :title="user.fullName">
+    <v-avatar size="30" color="blue lighten-1" :title="user.fullName">
       <v-img v-if="user.avatar" :src="user.avatar" />
       <span v-else class="white--text">{{ user.initials }}</span>
-      <v-icon size="10" :color="color" class="network">mdi-brightness-1</v-icon>
     </v-avatar>
+    <v-icon size="10" :color="color" class="network">mdi-brightness-1</v-icon>
   </div>
 </template>
 
